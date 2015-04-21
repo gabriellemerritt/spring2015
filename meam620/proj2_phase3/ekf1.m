a4af0ebd5b13672dfe7f374121aeabf8cddf2880
Gabrielle Merritt <gabrielle.merritt13@gmail.com>
2015-04-20 23:09:39 -0400

    ah

--- meam620/proj2_phase3/ekf1.m
+++ meam620/proj2_phase3/ekf1.m
@@ -38,70 +38,71 @@ function [X, Z] = ekf1(sensor, vic, varargin)
 %     the measurement should be in the following order
 %     [x; y; z; roll; pitch; yaw; other measurement you use]
 %     note that this output is optional, it's here in case you want to log your
-%     measurement 
+%     measurement
 persistent mu;  % previous state
 persistent sig; % previous covars
-persistent tv_old; 
+persistent tv_old;
 
 
-world_points = varargin{1}; 
-K = varargin{2}; 
-A_t1 = varargin{3}; 
-U_t1 = varargin{4}; 
-Xd = varargin{5}; 
+world_points = varargin{1};
+K = varargin{2};
+A_t1 = varargin{3};
+U_t1 = varargin{4};
+Xd = varargin{5};
 
-if(isempty(mu) && ~isempty(sensor)) 
+if(isempty(mu) && ~sensor.is_ready)
     %estimate initial pose from vicon vel readings
-    tv_old = vic.t; 
- 
-    [est_pos, est_eul]=estimate_pose(sensor,world_points,K); 
+    tv_old = vic.t;
+    
+    [est_pos, est_eul]=estimate_pose(sensor,world_points,K);
     X = [est_pos ;est_eul];
     Z = [est_pos ;est_eul];
-    sig = eye(6); 
-    mu = X; 
-else if (isempty(mu) && isempty(sensor)) 
-        X = []; 
+    sig = eye(6);
+    mu = X;
     
-else  %% need to make a case where we have 0 vicon readings or 0 sensor
-    if (~isempty(vic))
-   %% vars  
-        omega_m = vic.vel(4:6);  
-        v_m = vic.vel(1:3); 
-        dtv = vic.t - tv_old; 
-        phi = mu(4);
-        theta = mu(5);
-        psi = mu(6);
-        %% biases / noises
-
-       A_t= A_t1(0,0,0,phi,theta,omega_m(1),omega_m(2),omega_m(3)); 
-       U_t = U_t1(phi,theta); 
-
-    %% setting up for prediction
-       F_t = eye(6) + A_t*dtv;  
-       V_t = U_t*dtv;
-       X_d = Xd(0,0,0,0,0,0,phi,theta,v_m(1),v_m(2),v_m(3),omega_m(1),omega_m(2),omega_m(3)); 
-       Q = eye(6)*1; 
-    %% prediction 
-       mu = mu + X_d*dtv;   
-       sig = (F_t * sig *F_t')+ (V_t*Q*V_t');
-   end 
-%% update 
-   if(~isempty(sensor)) 
-        [est_pos, est_eul] = estimate_pose(sensor,world_points,K); 
-        z = [est_pos; est_eul]; 
-        C = eye(6); 
-        W = eye(6); 
-        R = eye(6); 
-
-        K_t = sig*C'/(C*sig*C' + W*R*W'); 
-        mu = mu + K_t*(z - C*mu); 
-        sig = sig - K_t*C*sig; 
-   end
-   %mu 6x1 
-X = mu;
-Z = z; 
-tv_old = vic.t;
-end
+else if (isempty(mu) && sensor.is_ready)
+        X = [];
+        
+    else  %% need to make a case where we have 0 vicon readings or 0 sensor
+        if (~isempty(vic))
+            %% vars
+            omega_m = vic.vel(4:6);
+            v_m = vic.vel(1:3);
+            dtv = vic.t - tv_old;
+            phi = mu(4);
+            theta = mu(5);
+            psi = mu(6);
+            %% biases / noises
+            
+            A_t= A_t1(0,0,0,phi,theta,omega_m(1),omega_m(2),omega_m(3));
+            U_t = U_t1(phi,theta);
+            
+            %% setting up for prediction
+            F_t = eye(6) + A_t*dtv;
+            V_t = U_t*dtv;
+            X_d = Xd(0,0,0,0,0,0,phi,theta,v_m(1),v_m(2),v_m(3),omega_m(1),omega_m(2),omega_m(3));
+            Q = eye(6)*1;
+            %% prediction
+            mu = mu + X_d*dtv;
+            sig = (F_t * sig *F_t')+ (V_t*Q*V_t');
+        end
+        %% update
+        if(~isempty(sensor))
+            [est_pos, est_eul] = estimate_pose(sensor,world_points,K);
+            z = [est_pos; est_eul];
+            C = eye(6);
+            W = eye(6);
+            R = eye(6);
+            
+            K_t = sig*C'/(C*sig*C' + W*R*W');
+            mu = mu + K_t*(z - C*mu);
+            sig = sig - K_t*C*sig;
+        end
+        %mu 6x1
+        X = mu;
+        Z = z;
+        tv_old = vic.t;
+    end
 end
 end
 
